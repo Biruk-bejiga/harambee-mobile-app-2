b484f6e382aa9f4bf3a14bbb1aad7652da0d2fb8
Biruk Bejga <birukbejga8@gmail.com>
2025-11-30 00:03:56 +0300

    chore: Downgrade nativewind and tailwindcss, and move nativewind/babel to plugins in babel config.

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,9 @@
 module.exports = function (api) {
     api.cache(true);
     return {
-        presets: [
-            "babel-preset-expo",
-            "nativewind/babel",
-        ],
+        presets: ["babel-preset-expo"],
         plugins: [
+            "nativewind/babel",
             "expo-router/babel",
             "react-native-reanimated/plugin",
         ],
