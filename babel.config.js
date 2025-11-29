531576a84a7669ed71047817eaf161ebdfed6b88
Biruk Bejga <birukbejga8@gmail.com>
2025-11-29 22:56:34 +0300

    feat: Add `react-native-worklets` dependency and simplify Babel preset configuration.

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,11 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [
-            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+            "babel-preset-expo",
             "nativewind/babel",
         ],
         plugins: [
-            // Required for expo-router
             'expo-router/babel',
-            // Reanimated plugin must be listed last
             'react-native-reanimated/plugin',
         ],
     };
